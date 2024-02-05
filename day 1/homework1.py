18c1b7b28efed8f4579cdb482d3915e97081fc9a
maghradzeirakli <maghradzeira@gmail.com>
2024-02-05 23:22:59 +0400

    second commit

---
+++ day 1/homework1.py
@@ -0,0 +1,9 @@
+name = "me var irakli"
+
+surname = "chemi gvaria mghradze"
+
+age = "var 14 wlis"
+
+height = " da var 174.2 sm"
+
+print((name + " " + surname + " " + (str(age) + " " + (str(height)))))
