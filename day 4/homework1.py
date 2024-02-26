b1b1fde50c8bba2ca5150064cc7eebdbbba20aa7
maghradzeirakli <maghradzeira@gmail.com>
2024-02-26 14:00:09 +0400

    fourth commit

---
+++ day 4/homework1.py
@@ -0,0 +1,10 @@
+name = input("enter your name: ")
+
+birth_year = int(input("enter your birth year: "))
+
+year =int(input("enter what year is it now: "))
+
+answer = year - birth_year
+
+print("hello" + " " + name)
+print("you are" + " " + str(answer) + " " + "years old")
---
+++ day 4/homework2.py
@@ -0,0 +1,10 @@
+name = input("enter your name: ")
+
+age = int(input("enter your age: "))
+
+future = int(input("How many years in the future do you want to travel?: "))
+
+answer = future + age
+
+print((name) + " " + "shen xar" + " " +str(age) + " " + "wlis" + " " + "da gida rom imogzauro momavalshi" + " " + str(future) + " " + "wlit")
+print("momavalshi shen iqnebi" + " " + str(answer) + " " + "wlis")
