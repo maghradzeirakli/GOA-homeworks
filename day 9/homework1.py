b2c72ad2fe919545e4bc00fa99463dd126781971
maghradzeirakli <maghradzeira@gmail.com>
2024-04-01 21:53:58 +0400

    ninth commit

---
+++ day 9/homework1.py
@@ -0,0 +1,8 @@
+salary = int(input("please enter your salary: "))
+
+if salary > 10000 :
+    print("goashi swavlobdi?")
+elif salary < 10000 and salary > 1000 :
+    print("you mid")
+elif salary < 1000 :
+    print("shemosuliyavi goashi matricelo") 
